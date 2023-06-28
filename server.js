5bc9d1c0b941561f23b96ed8311252c22ca46c35
arbi kustia <arbikustia14@gmail.com>
2023-06-28 12:57:19 +0700

    fox server.js

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const mongoose = require("mongoose");
 const userRouter = require("./App/routes/UserRouter");
 const activityRouter = require("./App/routes/ActivityRouter");
 const todoListRouter = require("./App/routes/TodoListRouter")
-
+const PORT = process.env.PORT || 3000;
 const app = express();
+
 
 const corsOrigin = {
   origin: "*",
@@ -18,29 +19,51 @@ app.use(cors(corsOrigin));
 app.use(express.json());
 
 // connect to database
-const DB = process.env.MONGO_URL || "test";
-mongoose
-  .connect(DB, {
-    usenewurlparser: true,
-    useunifiedtopology: true,
-  })
-  .then(() => {
-    console.log("Successfully connected ");
-  })
-  .catch((error) => {
-    console.log(`can not connect to database, ${error}`);
-  });
+const DB = process.env.MONGO_URL;
+// mongoose
+//   .connect(DB, {
+//     usenewurlparser: true,
+//     useunifiedtopology: true,
+//   })
+//   .then(() => {
+//     console.log("Successfully connected ");
+//   })
+//   .catch((error) => {
+//     console.log(`can not connect to database, ${error}`);
+//   });
+
+
+  const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(DB);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
+
 
 // create router
 app.use("/users", userRouter);
 app.use("/activity", activityRouter);
 app.use("/todolist", todoListRouter);
 
-app.get("/", (req, res) => {
-  res.send("TimeAce API");
-});
+app.all('*', (req,res) => {
+  res.json({"every thing":"is awesome"})
+})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () =>
-  console.log(`server running on port ${PORT}`)
-);
+// app.get("/", (req, res) => {
+//   res.send("TimeAce API");
+// });
+
+// app.listen(PORT, "0.0.0.0", () =>
+//   console.log(`server running on port ${PORT}`)
+// );
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+      console.log("listening for requests");
+  })
+})
